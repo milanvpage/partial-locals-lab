1841632aff9e9fb64e2c6980cbfc85643ee6573e
Milan <milanvpage@gmail.com>
2021-07-20 12:43:47 -0700

    commit tree 98d1306b67da10d1484cb2f75aa59234522d4ea0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,7 @@ class StudentsController < ApplicationController
 
   def edit
     @student = Student.find(params[:id])
+    @students = @classroom.students
   end
 
   def show
@@ -21,7 +22,8 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
+    render 'index'
   end
 
   def student_params
